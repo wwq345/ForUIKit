44a08f7423db4c8e04d6607fc286ee230ebcc224
wwq345 <wwq.bst@sap.com>
2022-05-09 11:15:34 +0800

    Initial Commit

---
+++ ForUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForUIKit
+//
+//  Created by i564206 on 2022/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
